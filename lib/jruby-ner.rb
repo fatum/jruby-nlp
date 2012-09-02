f8d55eb20374a416cea47edaa9f74edf2ffe8cfb
m.filippovich <m.filippovich@fun-box.ru>
2012-09-02 20:35:12 +0300

    fix NER_LIB path

--- lib/jruby-ner.rb
+++ lib/jruby-ner.rb
@@ -3,7 +3,7 @@ $:.unshift(NER_LIBS) unless
   $:.include?(NER_LIBS) || $:.include?(File.expand_path(NER_LIBS))
 
 require 'java'
-require 'src/stanford-ner.jar'
+require "#{NER_LIBS}/stanford-ner.jar"
 
 include_class "edu.stanford.nlp.ie.crf.CRFClassifier"
 
