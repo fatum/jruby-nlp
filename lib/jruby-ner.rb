b374534566fb17a948b0cbc95c7e2f11a2a67175
m.filippovich <m.filippovich@fun-box.ru>
2012-09-02 20:32:06 +0300

    fix NER_LIB path

--- lib/jruby-ner.rb
+++ lib/jruby-ner.rb
@@ -1,6 +1,7 @@
-NER_LIBS = File.dirname(__FILE__) + "/../../src" unless defined?(NER_LIBS)
+NER_LIBS = File.expand_path(File.dirname(__FILE__) + "/../src") unless defined?(NER_LIBS)
 $:.unshift(NER_LIBS) unless
   $:.include?(NER_LIBS) || $:.include?(File.expand_path(NER_LIBS))
+
 require 'java'
 require 'src/stanford-ner.jar'
 
